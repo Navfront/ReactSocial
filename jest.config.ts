9c0fd33df0f04bc5ef30f12e3f745aa20276e6c0
Andrey Nazhiganov <b3rt@yandex.ru>
2023-11-08 01:13:50 +0300

    Fix jest path mapper

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,9 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  modulePaths: ["<rootDir>/src"],
   moduleNameMapper: {
+    "^@src/(.*)$": "<rootDir>/src/$1",
     "\\.(scss)": "identity-obj-proxy",
   },
 };
